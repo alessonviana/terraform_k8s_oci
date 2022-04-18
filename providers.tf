50a12b826c24d13bf6e94c2a639d2583cc6e7c42
alesson.viana <a_viana@apple.com>
2022-04-18 17:38:33 -0300

    Revert "Add OCI Credentials"
    This reverts commit 2d33626dbf63ea724d718bdac4e0b0dcdf893a17.

--- providers.tf
+++ providers.tf
@@ -6,11 +6,3 @@ terraform {
     }
   }
 }
-
-provider "oci" {
-  tenancy_ocid = var.Tenancy_OCID
-  user_ocid = var.User_OCID
-  region = "us-ashburn-1"
-  private_key = var.PRIVATE_KEY
-  fingerprint = var.Fingerprint
-}
